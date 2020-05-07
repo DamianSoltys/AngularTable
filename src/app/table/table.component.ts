63a5c1797b8262d48574fd291c669ab973c949f4
DamianSoltys <soltysiukd@gmail.com>
2020-05-07 19:04:41 +0200

    [FEAT]: Add edit data and sorting functions

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ɵCompiler_compileModuleAndAllComponentsAsync__POST_R3__ } from '@angular/core';
 
 export interface TableData {
   id: number,
@@ -14,6 +14,15 @@ export interface TableConfig {
   pagination?: boolean,
 }
 
+interface EditableNode {
+  row: string,
+  column: string,
+}
+
+interface SortableColumn {
+
+}
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -23,24 +32,71 @@ export class TableComponent implements OnInit {
   @Input() config: TableConfig = null;
   public rowValues: string[] = [];
   public columnNames: string[] = [];
+  public editableNode: EditableNode = {
+    row: '',
+    column: '',
+  };
+  private sortableUp = {};
+  private prevSortable: string; 
 
   constructor() {}
 
   ngOnInit() {
     this.getColumnNames();
+    this.initialSort();
   }
 
   public sortByColumn(column) {
-    console.log(column);
+    if(column !== this.prevSortable) this.sortableUp[this.prevSortable] = false;
+
+    if(!this.sortableUp[column]) {
+      this.sortableUp[column] = true;
+    } else {
+      this.sortableUp[column] = false;
+    }
+    
+    this.config.data.sort((a,b) => {
+      if(a[column] > b[column] && this.sortableUp[column]) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    this.prevSortable = column;
   }
 
-  public editData(row, column) {
-    console.log(row);
-    console.log(column);
+  public editData(rowData, columnData) {
+    this.editableNode = {
+      row: rowData,
+      column: columnData,
+    }
   }
 
-  public canEdit(row, column) {
+  public clearEditableNode() {
+    this.editableNode = {
+      row: '',
+      column: '',
+    };
+  }
+
+  public canEdit(rowData, columnData) {
+    const { row, column } = this.editableNode;
+
+    if(row === rowData && column === columnData) return true;
     return false;
+  }
+
+  private initialSort() {
+    let initialValue;
+
+    this.config.data.sort();
+    this.prevSortable = this.columnNames[0];
+
+    this.columnNames.forEach((column, index)=> {
+      index !== 0? initialValue = false : initialValue = true;;
+      this.sortableUp[column] = initialValue;
+    });
   }
   
   private getColumnNames() {
